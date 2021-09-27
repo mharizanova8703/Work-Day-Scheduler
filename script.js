8fc7b22e9baa932902de46ae4e8b236727bc34b5
mharizanova8703 <mharizanova@yahoo.com>
2021-09-27 08:41:18 -0400

    updating on moment(Date)
    var current time=moment()

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // using moment for a day and time
 moment(Date)
 $('#currentDay').text(moment().format('ddd MMMM Do YYYY, h:mm a'))
+//moment current time
+var currentTime = moment()
+// Returns current time to the nearest hour - 12:30 becomes 12:00
+currentTime = currentTime.startOf('hour')
+// Calculates the start of day + 9 to return 9 am
+var beforeTime = moment().startOf('day').add(9, 'hours')
